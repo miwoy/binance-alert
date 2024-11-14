d9452fc11cdabb7ea78aa746723073a5251a2140
miwoy <miwoes@163.com>
2024-11-14 20:35:45 +0800

    debug

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function main() {
     const articles = initCache()
     log_with_time("Initialization complete. Monitoring new articles...");
 
-    let list = await getList()
+    // let list = await getList()
 
     // 首次获取缓存
     list.forEach(item => {
